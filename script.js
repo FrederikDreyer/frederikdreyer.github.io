c21acda1ade3dd6768a9442dcb00e0a6c4d9755f
FrederikDreyer <82033515+FrederikDreyer@users.noreply.github.com>
2021-10-07 09:01:42 +0200

    article lavet samt js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("det virker");
+
+function displayBlock() {
+  document.getElementById('article1').style.display = "block";
+}
+
+function displayNone() {
+  document.getElementById('article1').style.display = "none";
+}
